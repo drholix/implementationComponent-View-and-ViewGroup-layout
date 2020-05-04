4097f28316ec82f574547a5e4c97699261822572
drholix <drholix@gmail.com>
2020-05-04 23:32:42 +0700

    Initial commit

---
+++ app/src/main/java/id/awan/implementationcomponent_view_and_viewgroup_layout/MainActivity.java
@@ -0,0 +1,14 @@
+package id.awan.implementationcomponent_view_and_viewgroup_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
